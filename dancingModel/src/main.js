32adbce6ffcade57c5c4b011eaea5e3429f7c651
pissang <bm2736892@gmail.com>
2018-04-10 22:14:47 +0800

    Add dancing model example

---
+++ dancingModel/src/main.js
@@ -0,0 +1,23 @@
+import { application } from 'claygl';
+
+application.create('#main', {
+    width: 800,
+    height: 600,
+
+    graphic: {
+        // Enable shadow
+        shadow: true
+    },
+    init: function (app) {
+        // Create camera
+        this._camera = app.createCamera([0, 150, 200], [0, 100, 0]);
+
+        // Load model.
+        app.loadModel('../assets/models/SambaDancing/SambaDancing.gltf');
+
+        // Create lights
+        app.createDirectionalLight([-1, -1, -1], '#fff', 0.7);
+        app.createAmbientLight('#fff', 0.3);
+    },
+    loop: function (app) {}
+});
